eb7471b1e21dca1aa60bb2d6c7bc9f6a20675090
cold_hailstone <cksn1993@gmail.com>
2021-05-31 13:58:39 +0900

    유니온 타입과 인터섹션 타입의 차이점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -18,28 +18,33 @@ function logMessage(value: string | number): void {
 logMessage('hello');
 logMessage(100);
 
-interface Developer {
+interface Developer2 {
     name: string;
     skill: string;
 }
 
-interface Person {
+interface Person2 {
     name: string;
     age: number;
 }
 
 // 유니온
-// function askSomeone(someone: Developer | Person) {
+// function askSomeone(someone: Developer2 | Person2) {
 //     someone.name;
 //     // someone.skill
 //     // someone.age
 // }
+// askSomeone({ name: '디벨로퍼', skill: '웹 개발' });
+// askSomeone({ name: '캡틴', age: 100 });
+
 // 인터섹션
-function askSomeone(someone: Developer & Person) {
+function askSomeone(someone: Developer2 & Person2) {
     someone.name;
-    someone.skill
-    someone.age
+    someone.skill;
+    someone.age;
 }
+askSomeone({ name: '디벨로퍼', skill: '웹 개발', age: 25 });
+askSomeone({ name: '캡틴', age: 100, skill: '서버 개발' });
 
 // let seho4: string | number | boolean;
 // let capt4: string & number & boolean;
