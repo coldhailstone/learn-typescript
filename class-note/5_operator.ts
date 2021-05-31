a34c2003bb46bc2bf5f98433cee3a136014a6793
cold_hailstone <cksn1993@gmail.com>
2021-05-31 13:46:03 +0900

    유니온 타입의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -6,8 +6,14 @@
 // logMessage(false);
 
 // 유니온 타입 (하나 이상의 타입 사용)
+let seho3: string | number | boolean;
 function logMessage(value: string | number): void {
-    console.log(value);
+    if (typeof value === 'number')
+        value.toLocaleString();
+    else if (typeof value === 'string')
+        value.toString();
+    else
+        throw new TypeError('value must be string or number');
 }
 logMessage('hello');
 logMessage(100);
