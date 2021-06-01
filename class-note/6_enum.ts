0ccea8683780147a8f7f58ed063c093edf5fcac4
cold_hailstone <cksn1993@gmail.com>
2021-06-01 10:33:42 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,7 +1,15 @@
 enum Shoes {
-    Nike,
+    Nike = 10,
     Adidas
 }
 
 const myShoes = Shoes.Nike;
 console.log(myShoes);   // 0
+
+enum Shoes2 {
+    Nike = '나이키',
+    Adidas = '아디다스'
+}
+
+const myShoes2 = Shoes2.Nike;
+console.log(myShoes2);   // 나이키
