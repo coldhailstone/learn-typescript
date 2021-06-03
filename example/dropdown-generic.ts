332871a9040e9bac79f2c81c302f4b9e60454c38
cold_hailstone <cksn1993@gmail.com>
2021-06-03 10:07:58 +0900

    제네릭 실전 예제 살펴보기 - 코드에 타입 정의하기

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,37 @@
-const emails = [
+interface Email {
+  value: string,
+  selected: boolean
+}
+interface ProductNumber {
+  value: number,
+  selected: boolean
+}
+interface Option<T> {
+  value: T,
+  selected: boolean
+}
+
+const emails: Array<Option<string>> = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: Array<Option<number>> = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+// function createDropdownItem(item: Email | ProductNumber): HTMLOptionElement {
+//   const option = document.createElement('option');
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+function createDropdownItem<T>(item: Option<T>): HTMLOptionElement {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -19,8 +40,16 @@ function createDropdownItem(item) {
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
-emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+emails.forEach(email => {
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+});
+
+
+// NOTE: 상품 드롭 다운 아이템 추가
+numberOfProducts.forEach(product => {
+  const item = createDropdownItem<number>(product);
+  const selectTag = document.querySelector('#product-dropdown');
   selectTag.appendChild(item);
 });
