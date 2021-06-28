bfc923f3bc296cc2d8e430f136169c19872c9cc3
cold_hailstone <cksn1993@gmail.com>
2021-06-28 09:27:47 +0900

    타입 가드를 위한 예제 소개

---
+++ class-note/11_type-guard.ts
@@ -0,0 +1,27 @@
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function introduce(): Developer | Person {
+    return {
+        name: 'Tony',
+        age: 33,
+        skill: 'Iron Making'
+    }
+}
+let tony = introduce();
+console.log(tony.name);
+
+if ((tony as Developer).skill) {
+    let skill = (tony as Developer).skill;
+    console.log(skill);
+} else if ((tony as Person).age) {
+    let age = (tony as Person).age;
+    console.log(age);
+}
