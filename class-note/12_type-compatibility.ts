2dc73e3b7951d0ec7ff9be1b22d16f8d93d829f0
cold_hailstone <cksn1993@gmail.com>
2021-06-28 10:53:41 +0900

    타입 호환 예제 - 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -10,3 +10,30 @@ let developer: Developer2;
 let person: Person2;
 // developer = person;
 person = developer;
+
+// 함수
+var add = function(a: number) {
+    console.log(a);
+}
+var sum = function(a: number, b: number) {
+    return a + b;
+}
+// sum = add;
+// add = sum;
+
+// 제네릭
+interface Empty<T> {
+
+}
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+empty1 = empty2;
+empty2 = empty1;
+
+interface NotEmpty<T> {
+    data: T;
+}
+var notempty1: NotEmpty<string>;
+var notempty2: NotEmpty<number>;
+// notempty1 = notempty2;
+// notempty2 = notempty1;
