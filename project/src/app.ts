f910c18093827d0fa80289b5dc3e80bafe9c2a0a
cold_hailstone <cksn1993@gmail.com>
2021-07-14 15:28:55 +0900

    전세계 코로나 요약 정보 API 함수 관련 나머지 타입 정의

--- project/src/app.ts
+++ project/src/app.ts
@@ -241,12 +241,12 @@ function setCountryRanksByConfirmedCases(data: CovidSummaryResponse) {
   const sorted = data.Countries.sort(
     (a: Country, b: Country) => b.TotalConfirmed - a.TotalConfirmed
   );
-  sorted.forEach((value: any) => {
+  sorted.forEach((value: Country) => {
     const li = document.createElement('li');
     li.setAttribute('class', 'list-item flex align-center');
     li.setAttribute('id', value.Slug);
     const span = document.createElement('span');
-    span.textContent = value.TotalConfirmed;
+    span.textContent = value.TotalConfirmed.toString();
     span.setAttribute('class', 'cases');
     const p = document.createElement('p');
     p.setAttribute('class', 'country');
@@ -257,7 +257,7 @@ function setCountryRanksByConfirmedCases(data: CovidSummaryResponse) {
   });
 }
 
-function setLastUpdatedTimestamp(data: any) {
+function setLastUpdatedTimestamp(data: CovidSummaryResponse) {
   lastUpdatedTime.innerText = new Date(data.Date).toLocaleString();
 }
 
